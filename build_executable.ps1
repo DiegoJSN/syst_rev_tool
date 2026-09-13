fb1ec05add975c9303f6b67da24a8db8b540ef3f
Diego Soler <95866442+DiegoJSN@users.noreply.github.com>
2026-09-13 18:49:18 +0200

    Reduce antivirus false positives in Windows build

--- build_executable.ps1
+++ build_executable.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = "Stop"
 Set-Location $PSScriptRoot
 if (-not (Test-Path ".venv-build\Scripts\python.exe")) { python -m venv .venv-build }
-& ".venv-build\Scripts\python.exe" -m pip install --disable-pip-version-check -r requirements.txt pyinstaller==6.16.0
-& ".venv-build\Scripts\python.exe" -m PyInstaller --noconfirm --clean --onefile --console --name "SystRevTool-Demo" --add-data "templates;templates" --add-data "static;static" --exclude-module psycopg launcher.py
+& ".venv-build\Scripts\python.exe" -m pip install --disable-pip-version-check -r requirements.txt pyinstaller==6.20.0
+& ".venv-build\Scripts\python.exe" -m PyInstaller --noconfirm --clean --onedir --noupx --console --name "SystRevTool-Demo" --add-data "templates;templates" --add-data "static;static" --exclude-module psycopg launcher.py
 Write-Host ""
-Write-Host "Executable created at: dist\SystRevTool-Demo.exe"
+Write-Host "Portable folder created at: dist\SystRevTool-Demo"
 
