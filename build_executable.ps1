2989cfe0c67d77e5df1d7ccf115d2a4b0841e9e1
Diego Soler <95866442+DiegoJSN@users.noreply.github.com>
2026-09-13 17:46:25 +0200

    Add one-click Windows executable option

---
+++ build_executable.ps1
@@ -0,0 +1,8 @@
+$ErrorActionPreference = "Stop"
+Set-Location $PSScriptRoot
+if (-not (Test-Path ".venv-build\Scripts\python.exe")) { python -m venv .venv-build }
+& ".venv-build\Scripts\python.exe" -m pip install --disable-pip-version-check -r requirements.txt pyinstaller==6.16.0
+& ".venv-build\Scripts\python.exe" -m PyInstaller --noconfirm --clean --onefile --console --name "SystRevTool-Demo" --add-data "templates;templates" --add-data "static;static" --exclude-module psycopg launcher.py
+Write-Host ""
+Write-Host "Executable created at: dist\SystRevTool-Demo.exe"
+
