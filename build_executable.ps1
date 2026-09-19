0fee2b6f96b6f744b22d03895b6a7f337bd62099
Diego Soler <95866442+DiegoJSN@users.noreply.github.com>
2026-09-19 15:09:58 +0200

    Bundle example PDFs in Windows demo

--- build_executable.ps1
+++ build_executable.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 Set-Location $PSScriptRoot
 if (-not (Test-Path ".venv-build\Scripts\python.exe")) { python -m venv .venv-build }
 & ".venv-build\Scripts\python.exe" -m pip install --disable-pip-version-check -r requirements.txt pyinstaller==6.20.0
-& ".venv-build\Scripts\python.exe" -m PyInstaller --noconfirm --clean --onedir --noupx --console --name "SystRevTool-Demo" --add-data "templates;templates" --add-data "static;static" --exclude-module psycopg launcher.py
+& ".venv-build\Scripts\python.exe" -m PyInstaller --noconfirm --clean --onedir --noupx --console --name "SystRevTool-Demo" --add-data "templates;templates" --add-data "static;static" --add-data "example_pdfs;example_pdfs" --exclude-module psycopg launcher.py
 Write-Host ""
 Write-Host "Portable folder created at: dist\SystRevTool-Demo"
 
